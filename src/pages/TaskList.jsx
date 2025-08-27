e58c0e82141be1f90d9c78051562476b06e365c8
andrea-mosca <andrea.moscateli@gmail.com>
2025-08-27 15:10:10 +0200

    milestone 12

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,13 +1,29 @@
 import { useTaskContext } from "../context/GlobalContext";
 import TaskRow from "../component/TaskRow";
-import { useState, useMemo } from "react";
+import { useState, useMemo, useCallback } from "react";
+
+function debounce(callback, delay) {
+  let timer;
+  return (value) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback(value);
+    }, delay);
+  };
+}
 
 export default function TaskList() {
   const { tasks } = useTaskContext();
   const [sortBy, setSortBy] = useState("createdAt");
   const [sortOrder, setSortOrder] = useState(1);
+  const [searchQuery, setSearchQuery] = useState("");
 
+  const debouncedSearchSetQuery = useCallback(
+    debounce(setSearchQuery, 500),
+    []
+  );
   const sortIcon = sortOrder === 1 ? "⇩" : "⇧";
+
   const handleSort = (column) => {
     if (sortBy === column) {
       setSortOrder((prev) => -prev);
@@ -20,24 +36,33 @@ export default function TaskList() {
   const sortedTasks = useMemo(() => {
     const orderMap = { "To do": 1, Doing: 2, Done: 3 };
 
-    return [...tasks].sort((a, b) => {
-      let comparison = 0;
+    return [...tasks]
+      .filter((t) => t.title.toLowerCase().includes(searchQuery.toLowerCase()))
+      .sort((a, b) => {
+        let comparison = 0;
 
-      if (sortBy === "title") {
-        comparison = a.title.localeCompare(b.title);
-      } else if (sortBy === "status") {
-        comparison = orderMap[a.status] - orderMap[b.status];
-      } else if (sortBy === "createdAt") {
-        comparison =
-          new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
-      }
+        if (sortBy === "title") {
+          comparison = a.title.localeCompare(b.title);
+        } else if (sortBy === "status") {
+          comparison = orderMap[a.status] - orderMap[b.status];
+        } else if (sortBy === "createdAt") {
+          comparison =
+            new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+        }
 
-      return comparison * sortOrder;
-    });
-  }, [tasks, sortBy, sortOrder]);
+        return comparison * sortOrder;
+      });
+  }, [tasks, sortBy, sortOrder, searchQuery]);
+
   return (
     <div className="container">
       <h1>Tasks Lists</h1>
+      <div>
+        <input
+          onChange={(e) => debouncedSearchSetQuery(e.target.value)}
+          placeholder="Cerca una Task..."
+        ></input>
+      </div>
       <div className="mt-5">
         <table className="table border border-black p-2 mb-2 border-opacity-50">
           <thead>
